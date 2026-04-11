42f4869bbda88b1e863316cc14e59f9831a57667
Henry <henry9mulligan@gmail.com>
2026-04-11 09:12:37 -0600

    added base cli project

---
+++ NVP-API/Infrastructure/RavenDbService.cs
@@ -0,0 +1,24 @@
+﻿using Raven.Client.Documents;
+using Raven.Client.Documents.Session;
+
+namespace NVP.Infrastructure;
+
+public class RavenDbService
+{
+    private readonly IDocumentStore _store;
+
+    public RavenDbService(IDocumentStore store)
+    {
+        _store = store;
+    }
+
+    public IDocumentSession OpenSession()
+    {
+        return _store.OpenSession();
+    }
+
+    public IAsyncDocumentSession OpenAsyncSession()
+    {
+        return _store.OpenAsyncSession();
+    }
+}
